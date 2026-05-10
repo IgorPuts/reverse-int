b2ab32f2bbb761f0cfb93891fb8e2598b604baf3
IgorPuts <exNeron@yandex.ru>
2026-05-11 01:54:57 +0300

    fix: delete spaces

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 module.exports = function reverse(n) {
-    let number = `${Math.abs(n)}` + '';
-    let string = '';
-    for (let i = number.length - 1; i >= 0; i--) {
-      string += number[i];
-    }
-    return `${+string}`;
+  let number = `${Math.abs(n)}` + '';
+  let string = '';
+  for (let i = number.length - 1; i >= 0; i--) {
+    string += number[i];
+  }
+  return `${+string}`;
 };
+const abs = String(Math.abs(n));
+  const arr1 = abs.split('');
+  const arr2 = arr1.reverse();
+  const result = arr2.join('');
+  const reversnum = Number(result);
+  return reversnum;
