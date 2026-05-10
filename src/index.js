35ec651c9d6e79cb8511e3b242c0bbc694464382
IgorPuts <exNeron@yandex.ru>
2026-05-11 01:49:16 +0300

    refactor: change arch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function reverse(n) {
-    let number = Math.abs(n) + '';
+    let number = `${Math.abs(n)}` + '';
     let string = '';
     for (let i = number.length - 1; i >= 0; i--) {
       string += number[i];
     }
-    return Math.abs(+string);
+    return `${+string}`;
 };
