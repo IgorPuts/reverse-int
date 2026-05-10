f524633a07541d65ec7eaf0a95772f4a0f0ca180
IgorPuts <exNeron@yandex.ru>
2026-05-11 01:43:48 +0300

    feat: update reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+    let number = Math.abs(n) + '';
+    let string = '';
+    for (let i = number.length - 1; i >= 0; i--) {
+      string += number[i];
+    }
+    return Math.abs(+string);
 };
+
+
